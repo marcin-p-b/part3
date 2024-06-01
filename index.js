02a779396663842ae1bce8fd92e51f01056d6066
Your Name <you@example.com>
2024-06-01 22:05:14 +0200

    uibuild

--- index.js
+++ index.js
@@ -191,8 +191,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
-    console.log(error.message);
-    return response.status(400).send({ error: error.message });
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
