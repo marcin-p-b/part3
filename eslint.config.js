eb022748148abd19764bc0cbf09a4b1f85c8b3b9
Your Name <you@example.com>
2024-06-03 22:44:41 +0200

    uibuild

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,21 @@ import pluginJs from "@eslint/js";
 
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { sourceType: "module", globals: globals.browser } },
   pluginJs.configs.recommended,
   {
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error",
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "single"],
+      semi: ["error", "never"],
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-console": 0,
     },
   },
 ];
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-"use strict";
-import express, { request } from "express";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import cors from "cors";
-import "dotenv/config";
-import Person from "./models/person.js";
+'use strict'
+import express, { request } from 'express'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+import 'dotenv/config'
+import Person from './models/person.js'
 
-const app = express();
-const port = process.env.PORT;
+const app = express()
+const port = process.env.PORT
 
-app.use(express.json());
-app.use(express.static("dist"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
-let personcount = 0;
+let personcount = 0
 
 // let persons = [
 //   {
@@ -43,19 +43,19 @@ let personcount = 0;
 //   },
 // ];
 
-let infoDate = new Date();
+let infoDate = new Date()
 
-app.get("/", (request, response) => {
-  response.send("");
-});
+app.get('/', (request, response) => {
+  response.send('')
+})
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
-    response.json(persons);
-  });
-});
+    response.json(persons)
+  })
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   // Person.find().then((person) => console.log(person[0]["name"]));
   // Person.find({})
   //   .then((person) => {
@@ -74,33 +74,33 @@ app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   Person.find({}).then((persons) => {
-    personcount = persons.length;
+    personcount = persons.length
     response.send(
       `<p>Phonebook has info for ${personcount}
        people</p><p>${infoDate}</p>`
-    );
-  });
-});
+    )
+  })
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
-  console.log(request.params.id);
+app.delete('/api/persons/:id', (request, response, next) => {
+  console.log(request.params.id)
   Person.findByIdAndDelete(request.params.id)
     .then((person) => {
-      response.json(person);
-      response.status(204).end();
+      response.json(person)
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // const generateId = () => {
 //   const maxId =
@@ -148,9 +148,9 @@ app.delete("/api/persons/:id", (request, response, next) => {
 //   }
 // });
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
-  console.log(body);
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  console.log(body)
 
   // if (body.name === undefined || body.number === undefined) {
   //   console.log(process.argv);
@@ -160,44 +160,44 @@ app.post("/api/persons", (request, response, next) => {
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
   const person = {
     name: body.name,
     number: body.number,
-  };
+  }
 
   Person.updateOne({ name: person.name }, { number: person.number })
     .then((updatedPerson) => {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-app.listen(port);
-console.log(`Server running on port ${port}`);
+app.listen(port)
+console.log(`Server running on port ${port}`)
